0c9be19c3e7efeddd284f31aaf51616c9081104b
Maria Julia <maria.espinoza@alumnos.frm.utn.edu.ar>
2022-10-20 14:39:38 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*Crear un método que permita calcular el área de un rectángulo.*/
+            //Formula -> Base x Altura 
+
+
+
+        }
+    }
+}
